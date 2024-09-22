252462863738bded344e1ab559b615b6a43913bf
FrancoJack123 <jackfrancomoreno15@gmail.com>
2024-09-22 18:25:18 -0500

    Iniciación del proyecto backend

---
+++ src/main/java/pe/edu/cibertec/autobackendb/AutoBackendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.autobackendb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBackendBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoBackendBApplication.class, args);
+    }
+
+}
